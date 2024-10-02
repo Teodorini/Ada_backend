307fc49546b2cffc7933b7a936c0653360afd9ab
teodorini <marianateo888@gmail.com>
2024-10-02 18:10:44 -0300

    Elimine carpeta no necesaria

--- modulo4_git/clase38/ejemplos/clase35/mercadoAbierto/ejercicio1/views.js
+++
@@ -1,7 +0,0 @@
-
-// Función para mostrar mensajes en la consola del servidor
-function displayMessage(message) {
-    console.log(`Servidor: ${message}`);
-}
-
-module.exports = { displayMessage };
